449edafafc5e3bc6f666325b57a601c4421eb157
Ednilson Albuquerque <ednilson.albuquerque@conexasaude.com.br>
2022-01-23 14:19:03 -0400

    chore: create internal api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,40 @@
+import { createServer, Model } from 'miragejs';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [{
+        id: 1,
+        title: 'Freelancer de website',
+        type: 'deposit',
+        category: 'Dev',
+        amount: 6000,
+        createdAt: new Date('2022-06-01 09:00:00')
+      }]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api'
+    
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
